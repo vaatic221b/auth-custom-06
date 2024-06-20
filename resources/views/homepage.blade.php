745c3dc438e5877acdbb09bb34b014f77b341391
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-06-20 20:30:21 +0800

    Activity 4 - Sucalit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,13 @@
+@extends('mainLayout')
+
+@section('page-title', 'Main Landing Page')
+
+@section('page-content')
+<h1>Welcome to the Site</h1>
+<br>
+{{-- <a href="{{ route('logout') }}">Logout</a> --}}
+<form action="{{ route('logout') }}" method="POST">
+    @csrf
+    <button type="submit">Logout</button>
+</form>
+@endsection
